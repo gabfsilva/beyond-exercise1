1db8e44526a611568ab254f8e45e831d7373faa0
Gabriel Ferreira <108829234+gabfsilva@users.noreply.github.com>
2023-09-25 20:27:39 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/hello-world.gif">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+* {
+  background-color: #1c1d1d;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #92dd85;
+  margin-top: 60px;
+}
+
+p {
+  font-size: 1.2rem;
+}
+</style>
